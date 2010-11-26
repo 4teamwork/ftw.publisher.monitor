8994e0a97926a4b6886c5bf1b5b3189cdc8229bf
Jonas Baumann <j.baumann@4teamwork.ch>
2010-11-26 11:24:54 +0000

    pastered package

---
+++ README.txt
@@ -0,0 +1,5 @@
+Introduction
+============
+
+`ftw.publisher.monitor` monitors the publisher queue and alerts when
+the queue cannot be processed for some reason.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+maintainer = 'Jonas Baumann'
+
+setup(name='ftw.publisher.monitor',
+      version=version,
+      description="Publisher monitoring system for monitoring the queue" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='ftw publisher monitoring',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/dist/ftw-publisher-monitor',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.publisher'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
