c11027077992377a87cefaf85d492dec5c9f7ffa
Jonas Baumann <jone@jone.ch>
2012-11-02 15:54:26 +0100

    Move email_addresses_validator to utils.

---
+++ ftw/publisher/monitor/tests/test_utils.py
@@ -0,0 +1,32 @@
+from ftw.publisher.monitor import utils
+from ftw.testing import MockTestCase
+
+
+class TestEmailAddressValidator(MockTestCase):
+
+    def test_valid_email(self):
+        self.assertTrue(utils.email_addresses_validator('hugo.boss@web.de'))
+
+    def test_valid_multiline_emails(self):
+        self.assertTrue(utils.email_addresses_validator('\n'.join((
+                        'hugo.boss@web.de',
+                        'hugo@boss.com'))))
+
+    def test_number_emails(self):
+        self.assertTrue(utils.email_addresses_validator('1my@mail.ch'))
+        self.assertTrue(utils.email_addresses_validator('info@4teamwork.ch'))
+
+    def test_special_emails(self):
+        self.assertTrue(utils.email_addresses_validator(
+                'my-very.special-mail@ver.y.spec.ial.do.main.com'))
+
+    def test_local_addresses(self):
+        self.assertTrue(utils.email_addresses_validator('me@home.local'))
+        self.assertFalse(utils.email_addresses_validator('me@local'))
+
+    def test_invalid_addresses(self):
+        self.assertFalse(utils.email_addresses_validator('invalid'))
+        self.assertFalse(utils.email_addresses_validator('\n'.join((
+                        'hugo@boss.com',
+                        'invalid',
+                        'hugo@boss.com'))))
